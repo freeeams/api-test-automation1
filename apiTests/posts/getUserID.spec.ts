d2d593fbc47426dee4240d8fbaeb395647acdd8b
freeeams <freams.fox@gmail.com>
2025-10-09 20:41:44 -0500

    getAllComments

---
+++ apiTests/posts/getUserID.spec.ts
@@ -0,0 +1,56 @@
+import { test, expect } from '@playwright/test'
+import z from 'zod';
+
+const userSchema = z.object({
+    id: z.number().min(0),
+    name: z.string(),
+    username: z.string(),
+    email: z.string(),
+    address: z.object({
+        street: z.string(),
+        suite: z.string(),
+        city: z.string(),
+        zipcode: z.string(),
+        geo: z.object({
+            lat: z.string(),
+            lng: z.string()
+        })
+    }),
+    phone: z.string(),
+    website: z.string(),
+    company: z.object({
+        name: z.string(),
+        catchPhrase: z.string(),
+        bs: z.string()
+    })
+});
+
+const baseURL = process.env.baseURL;
+test('Get user by ID', async ({ request }) => {
+
+    const userId = 3;
+    const response = await request.get(`${baseURL}/users/${userId}`);
+    expect(response.status()).toBe(200);
+    const responseBody = await response.json();
+    const parsedSchema = userSchema.parse(responseBody);
+    console.log(parsedSchema);
+});
+
+test('Get all users and validate first user details', async ({ request }) => {
+    const getAllUsersResponse = await request.get(`${baseURL}/users`)
+    const getAllUsersResponseJSON = await getAllUsersResponse.json()
+    const firstUserId = getAllUsersResponseJSON[0].id;
+
+    
+    const userResponse = await request.get(`${baseURL}/users/${firstUserId}`)
+    const userResponseJSON = await userResponse.json()
+    userSchema.parse(userResponseJSON);
+})
+
+test('Get all comments from a post with post id', async ({ request }) => {
+    const postId = 1;
+    const response = await request.get(`${baseURL}/posts/${postId}/comments`);
+    expect(response.status()).toBe(200);
+    const responseBody = await response.json();
+    console.log(responseBody);
+}); 
