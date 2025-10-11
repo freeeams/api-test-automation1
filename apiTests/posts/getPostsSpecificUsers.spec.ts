2ce5e4e54777ce91f71d0d9abd61823d1c379dda
freeeams <freams.fox@gmail.com>
2025-10-10 20:35:53 -0500

    newPost

--- apiTests/posts/getPostsSpecificUsers.spec.ts
+++ apiTests/posts/getPostsSpecificUsers.spec.ts
@@ -21,3 +21,21 @@ test('Get posts for specific user', async ({ request }) => {
         postSchema.parse(responseJSON[i]);
     }
 });
+
+test('Create a new post', async ({ request }) => {
+    const newPostRequestBody = {
+        "userId": 1,
+        "title": "delectus aut autem",
+        "body": "quis ut nam facilis et officia qui"
+    }
+    const response = await request.post(`${baseURL}/posts`, { data: newPostRequestBody });
+    expect(response.status()).toBe(201)
+    const expectedResponseSchema = z.object({
+        userId: z.literal(newPostRequestBody.userId),
+        id: z.number().positive(),
+        title: z.literal(newPostRequestBody.title),
+        body: z.literal(newPostRequestBody.body)
+    });
+    const actualResponseJSON = await response.json()
+    expectedResponseSchema.parse(actualResponseJSON);
+});
